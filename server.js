94e4b38f13c3dc68c2a02e49f746e69bb9130137
rushg171 <58556123+rushg171@users.noreply.github.com>
2022-09-19 19:25:49 +0530

    CORS added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const { proRouter } = require('./router');
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use('/products', proRouter);
 
 app.get('/', (req, res) => {
